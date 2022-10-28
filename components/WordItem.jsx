5588a289b0d2de25bf5fec56d7b5d24f2c48452f
yair-roshal <yair7705778@gmail.com>
2022-10-28 13:51:39 +0200

    update WordItem.jsx

--- components/WordItem.jsx
+++ components/WordItem.jsx
@@ -2,48 +2,39 @@ import React from 'react'
 import { Link } from 'components'
 import axios from 'axios'
 import IconButton from '@mui/material/IconButton'
- import DeleteIcon from '@mui/icons-material/Delete';
-import RefreshIcon from '@mui/icons-material/Refresh';
-import OpenInNewIcon from '@mui/icons-material/OpenInNew';
-import { DeleteButton, ShowButton, ExportButton, UpdateButton } from './styledButtons'
+import DeleteIcon from '@mui/icons-material/Delete'
+import RefreshIcon from '@mui/icons-material/Refresh'
+import OpenInNewIcon from '@mui/icons-material/OpenInNew'
 
 export const WordItem = ({ word }) => {
 	const { id, original, translate } = word
 
-	// const deleteWord = id => {
-	//     axios
-	//         .delete('http://localhost:5000/' + id)
-	//         .then(res => {
-	//             if (res.status === 200) {
-	//                 alert('Word successfully deleted')
-	//                 window.location.reload()
-	//             } else Promise.reject()
-	//         })
-	//         .catch(err => alert('Something went wrong'))
-	// }
+	const deleteWord = (id) => {
+		axios
+			.delete('http://localhost:5000/' + id)
+			.then((res) => {
+				if (res.status === 200) {
+					alert('Word successfully deleted')
+					window.location.reload()
+				} else Promise.reject()
+			})
+			.catch((err) => alert('Something went wrong'))
+	}
 
 	return (
 		<td className='cell'>
 			<div className='edit'>
 				<Link href={`/words/update/${id}`} style={{ textDecoration: 'none' }}>
-					{/* <UpdateButton variant='contained'>Update</UpdateButton> */}
-
-					<IconButton color='primary' aria-label='upload picture' component='label'>
+					<IconButton color='secondary' component='label'>
 						<RefreshIcon />
 					</IconButton>
 				</Link>
 
-				{/* <DeleteButton onClick={() => deleteWord(id)} variant='contained'>
-					Delete
-				</DeleteButton> */}
-
-				<IconButton  onClick={() => deleteWord(id)}  color='primary' aria-label='upload picture' component='label'>
-						<DeleteIcon />
-					</IconButton>
-
+				<IconButton onClick={() => deleteWord(id)} aria-label='delete'>
+					<DeleteIcon />
+				</IconButton>
 
 				<Link href={`/words/show/${id}`} style={{ textDecoration: 'none' }}>
-					{/* <ShowButton variant='contained'>Show</ShowButton> */}
 					<IconButton color='primary' aria-label='upload picture' component='label'>
 						<OpenInNewIcon />
 					</IconButton>
