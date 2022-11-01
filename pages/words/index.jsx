530d445c5fa039234edc093e50746f514f216b9a
yair-roshal <yair7705778@gmail.com>
2022-11-02 00:26:09 +0200

    update 5 files and delete 1 file

--- pages/words/index.jsx
+++ pages/words/index.jsx
@@ -4,37 +4,37 @@ import { AllWords } from 'components'
 import { Loading } from 'components'
 import { Header } from 'components'
 import axios from 'axios'
+import store from './redux/store'
+import { Provider } from 'react-redux'
 
 export default function Index() {
-    const [words, setWords] = useState(null)
-    const URL = 'http://localhost:5000/'
+	const [words, setWords] = useState(null)
+	const URL = 'http://localhost:5000/'
 
-    useEffect(() => {
-        axios
-            .get(URL)
-            .then(response => {
-                setWords(response.data)
-            })
-            .catch(error => {
-                console.log(error)
-            })
-    }, [])
+	useEffect(() => {
+		axios
+			.get(URL)
+			.then((response) => {
+				setWords(response.data)
+			})
+			.catch((error) => {
+				console.log(error)
+			})
+	}, [])
 
-    if (!words) {
-        return <Loading />
-    }
+	if (!words) {
+		return <Loading />
+	}
 
-    return (
-        <div>
-            <h1 className='titlePage'>Word</h1>
-            <div className='wrapperAllWords'>
-                <Header />
-                <AllWords words={words} />
-
-
-
-
-            </div>
-        </div>
-    )
+	return (
+		<Provider store={store}>
+			<div>
+				<h1 className='titlePage'>Word</h1>
+				<div className='wrapperAllWords'>
+					<Header />
+					<AllWords words={words} />
+				</div>
+			</div>
+		</Provider>
+	)
 }
