e8b240b6f17b6c5beb5fcb18eb94c06ade21e4fe
Yair Roshal <Yair7705778@gmail.com>
2022-11-23 21:22:27 +0200

    build(deps): update .eslintrc.json, AddWord.jsx, jsconfig.json and package-lock.json

--- components/crudComponents/AddWord.jsx
+++ components/crudComponents/AddWord.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { Link } from 'components'
 import { useForm } from 'react-hook-form'
 import { UpdateButton } from 'components'
