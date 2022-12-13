ca666e1b316bd2c574f25145da2c44e16d53e40d
yair-roshal <yair7705778@gmail.com>
2022-12-13 12:26:13 +0200

    update 11 files and delete 1 file

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,19 +1,8 @@
 import { AppBar, Toolbar, CssBaseline } from '@mui/material/'
 import { NavLink } from 'components'
- import { pagesPrayers } from '../constants/clientConstants'
- 
- 
-export const Navbar = () => {
-    // const pagesPrayers = [
-    //     { name: 'Shaharit', href: '/shaharit' },
-    //     { name: 'Minha', href: '/minha' },
-    //     { name: 'Maariv', href: '/maariv' },
-    //     { name: 'Main shalosh', href: '/main-shalosh' },
-    //     { name: 'Birkat aMazon', href: '/birkat-amazon' },
-    //     { name: 'Tehilim', href: '/tehilim' },
-    // ]
-    // console.log('pagesPrayers222 :>> ', pagesPrayers)
+import { pagesPrayers } from '../constants/clientConstants'
 
+export const Navbar = () => {
     return (
         <>
             <AppBar position='static'>
@@ -30,20 +19,6 @@ export const Navbar = () => {
                                 {pagesPrayer.name}
                             </NavLink>
                         ))}
-{/* 
-                        <NavLink href='/' exact className='link' style={{ textDecoration: 'none' }}>
-                            Home
-                        </NavLink>
-
-                        <NavLink href='/words' className='link' style={{ textDecoration: 'none' }}>
-                            Tehilim
-                        </NavLink>
-                        <NavLink href='/words' className='link' style={{ textDecoration: 'none' }}>
-                            Shaharit
-                        </NavLink>
-                        <NavLink href='/words' className='link' style={{ textDecoration: 'none' }}>
-                            Minha
-                        </NavLink> */}
                     </div>
                 </Toolbar>
             </AppBar>
