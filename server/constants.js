f9b912a411fdd78995d8466de11a6baab0eb919d
Yair770 <yair7705778@gmail.com>
2022-12-19 00:41:52 +0200

    fix bugs

--- server/constants.js
+++ server/constants.js
@@ -2,8 +2,11 @@ module.exports = Object.freeze({
     sqlConfig: {
         connectionLimit: 10,
         host: 'localhost',
-        user: 'foo',
-        password: 'foo',
+        user: 'root',
+        password: '',
+        // user: 'foo',
+        // password: 'foo',
+
         database: 'under-text',
     },
     ANOTHER_CONSTANT: 'another value',
