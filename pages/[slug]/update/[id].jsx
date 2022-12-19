2d0e689ffb376eb8e5fccca5779c8783788b886a
Yair770 <yair7705778@gmail.com>
2022-12-19 21:35:35 +0200

    fix bugs

--- pages/[slug]/update/[id].jsx
+++ pages/[slug]/update/[id].jsx
@@ -3,10 +3,7 @@ import { URL } from 'constants/clientConstants'
 import axios from 'axios'
 
 export async function getServerSideProps({ params }) {
-    console.log('`${URL}/${params.slug}/${params.id}`', `${URL}/${params.slug}/${params.id}`)
-    // const response = await axios.get(`${URL}/shaharit/${params.id}`)
     const response = await axios.get(`${URL}/${params.slug}/${params.id}`)
-
 
     return {
         props: { ...response.data }[0],
