00d4556582ba67fb747fc356fc8268a96017262a
Yair770 <yair7705778@gmail.com>
2022-12-25 18:11:49 +0200

    fix bugs

--- components/Link.jsx
+++ components/Link.jsx
@@ -1,9 +1,13 @@
 import NextLink from 'next/link'
 
-export function Link({ href, children, ...props }) {
+export function Link({ href, children }) {
+    // export function Link({ href, children, ...props }) {
     return (
-        <NextLink href={href}>
-            <a className='styledLink' {...props}>{children}</a>
+        <NextLink className='styledLink' href={href}>
+            {/* <a className='styledLink' 
+            {...props}
+            >{children}</a> */}
+            {children}
         </NextLink>
     )
 }
--- next.config.js
+++ next.config.js
@@ -12,11 +12,12 @@ module.exports = {
 
 
     // Next 12.1.x or earlier
+    // experimental: {
+    //     outputStandalone: true,
+    //   }
 
-    experimental: {
-        outputStandalone: true,
-      },
-          // output: 'standalone'  -  12.2.x or later
+    //   -  12.2.x or later
+          output: 'standalone'  
 
 }
  
