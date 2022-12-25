8b97946f1adf8b342f8f5af3fbb5358cba3f828b
Yair770 <yair7705778@gmail.com>
2022-12-25 15:26:33 +0200

    fix bugs

--- next.config.js
+++ next.config.js
@@ -8,4 +8,15 @@ module.exports = {
     env: {
         PORT: process.env.PORT,
     },
+
+
+
+    // Next 12.1.x or earlier
+
+    experimental: {
+        outputStandalone: true,
+      },
+          // output: 'standalone'  -  12.2.x or later
+
 }
+ 
