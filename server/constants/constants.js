03ae0675f851d658a3bf1b17282702b322dea065
Yair770 <yair7705778@gmail.com>
2023-06-21 18:04:07 +0300

    fix

--- server/constants/constants.js
+++ server/constants/constants.js
@@ -1,11 +1,23 @@
 module.exports = Object.freeze({
-    sqlConfig: {
-        connectionLimit: 10,
-        host: 'localhost',
-        user: 'root',
-        // password: '',
-        password: 'root',
-        database: 'under-text',
-    },
-    ANOTHER_CONSTANT: 'another value',
+  sqlConfig: {
+    connectionLimit: 10,
+    host: '194.31.175.248',
+    port: '3306',
+    user: 'gen_user',
+    password: 'pw07613vrm1',
+    database: 'default_db',
+  },
+  ANOTHER_CONSTANT: 'another value',
 })
+
+// module.exports = Object.freeze({
+//     sqlConfig: {
+//         connectionLimit: 10,
+//         host: 'localhost',
+//         user: 'root',
+//         // password: '',
+//         password: 'root',
+//         database: 'under-text',
+//     },
+//     ANOTHER_CONSTANT: 'another value',
+// })
