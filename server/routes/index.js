518023196be56fa2ee5d22d29e192258d1b79d73
yair-roshal <yair7705778@gmail.com>
2023-07-24 15:04:30 +0300

    Merge remote-tracking branch 'origin/master'

--- server/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express'),
-    router = express.Router(),
-    wordsRoutes = require('./words.routes')
-
-router.use('/', wordsRoutes)
-// router.use('/words', wordsRoutes)
-
-module.exports = router
--- server/utils/utils.js
+++
@@ -1,7 +0,0 @@
-const replaceStr = (str, char, replacer) => {
-    const regex = new RegExp(char, 'g')
-    const replaced = str.replace(regex, replacer)
-    return replaced
-}
-
-module.exports = { replaceStr }
