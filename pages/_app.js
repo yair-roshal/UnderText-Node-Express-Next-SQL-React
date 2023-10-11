89808bddda9c1fde8c3bfb98de929698810148a4
yair-roshal <yair7705778@gmail.com>
2023-10-11 19:15:02 +0300

    delete 1 file and update 1 file

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,21 @@
 import Head from 'next/head'
 import 'styles/App.scss'
 import { MenuBar } from 'components'
- 
-export default function App({ Component, pageProps }) {
-    return (
-        <>
-                 <Head>
-                    <title>Under Text</title>
-                    <link rel='shortcut icon' href='/favicon.ico' />
-                </Head>
 
-                <div>
-                    <MenuBar />
-                    <div>
-                        <Component {...pageProps} />
-                    </div>
-                </div>
-         </>
-    )
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>Under Text</title>
+        <link rel='shortcut icon' href='/favicon.ico' />
+      </Head>
+
+      <div>
+        <MenuBar />
+        <div>
+          <Component {...pageProps} />
+        </div>
+      </div>
+    </>
+  )
 }
