498da4a4f86c44a0cf1d6a35e4a3304b1ee0a57a
yair-roshal <yair7705778@gmail.com>
2023-10-11 14:24:21 +0300

    build(deps): update next.config.js, package-lock.json and package.json

--- next.config.js
+++ next.config.js
@@ -1,22 +1,13 @@
 const path = require('path')
- 
+
 module.exports = {
-    
- 
-    sassOptions: {
-        includePaths: [path.join(__dirname, 'styles')],
-    },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
 
-      
-    //   -  12.2.x or later
-    output: 'standalone',
+  output: 'standalone',
 
-    // Next 12.1.x or earlier
-    // experimental: {
-    //     outputStandalone: true,
-    //   }
-
-    env: {
-        PORT: process.env.PORT,
-    },
+  env: {
+    PORT: process.env.PORT,
+  },
 }
