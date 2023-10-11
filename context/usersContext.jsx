344485bbbac0fdc0502931cac14700d804ac5bc4
yair-roshal <yair7705778@gmail.com>
2023-10-12 01:28:52 +0300

    update 2 files and delete 1 file

--- context/usersContext.jsx
+++ context/usersContext.jsx
@@ -1,43 +1,43 @@
-import { createContext, useContext, useState, useEffect, useMemo } from 'react';
-import initialUsersData from '../data/initialUsersData.json';
+import { createContext, useContext, useState, useEffect, useMemo } from 'react'
+import initialUsersData from '../constants/initialUsersData.json'
 
 const UsersContext = createContext({
   usersData: initialUsersData || [],
   setUsersData: () => {},
   loading: true,
-});
+})
 
 export const ContextProvider = ({ children }) => {
-  const [usersData, setUsersData] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [usersData, setUsersData] = useState([])
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     // Попытка получить данные из localStorage
-    const storedData = localStorage.getItem('usersData');
+    const storedData = localStorage.getItem('usersData')
 
     if (storedData) {
-      setUsersData(JSON.parse(storedData));
+      setUsersData(JSON.parse(storedData))
     } else {
       // Если данных в localStorage нет, используем начальные данные
-      setUsersData(initialUsersData);
+      setUsersData(initialUsersData)
     }
 
-    setLoading(false);
-  }, []);
+    setLoading(false)
+  }, [])
 
   // Сохранение данных в localStorage при изменении usersData
   useEffect(() => {
-    localStorage.setItem('usersData', JSON.stringify(usersData));
-  }, [usersData]);
+    localStorage.setItem('usersData', JSON.stringify(usersData))
+  }, [usersData])
 
   const contextValue = useMemo(
     () => ({ usersData, setUsersData, loading }),
     [usersData, setUsersData, loading],
-  );
+  )
 
-  return <UsersContext.Provider value={contextValue}>{children}</UsersContext.Provider>;
-};
+  return <UsersContext.Provider value={contextValue}>{children}</UsersContext.Provider>
+}
 
-export const useUsersContext = () => useContext(UsersContext);
+export const useUsersContext = () => useContext(UsersContext)
 
-export default UsersContext;
+export default UsersContext
